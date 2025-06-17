7bf231478c4f3b8698e4e97eb7124dc63f5e4403
Basit Adebayo <bahseet2022@gmail.com>
2025-06-18 00:12:44 +0100

    Updated the app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,23 @@
-import express from "express"
-import dotenv from "dotenv"
-import {signUpRoutes} from "./routes/userroutes.js"
+import express from "express";
+import dotenv from "dotenv";
+import sequelize from "./configs/dbconfig.js";
+import { signUpRoutes } from "./routes/userroutes.js";
+import { Sequelize } from "sequelize";
 
-dotenv.config()
+dotenv.config();
 
-app.use("/healthcircuit", signUpRoutes )
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use("/healthcircuit", signUpRoutes);
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error syncing database: ", err);
+  });
