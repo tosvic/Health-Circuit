c6b5b60e2e257a25e8ac88e84e7b475a130dc3c8
David Olabisi <davbrown2005@gmail.com>
2025-06-19 22:44:44 +0100

    update

--- assets/js/publichealth.js
+++
@@ -1,9 +0,0 @@
-fetch('/components/nav.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('nav-placeholder').innerHTML = html;
-    // Dynamically load nav.js after nav.html is injected
-    const script = document.createElement('script');
-    script.src = '/components/nav.js';
-    document.body.appendChild(script);
-  });
